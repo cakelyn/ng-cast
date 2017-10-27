41fa89063b3a54b955803b651a8c6b39fe475a47
Katelyn Stamas <cakelyn@gmail.com>
2017-10-27 16:40:57 -0400

    rendered videoListEntry.js

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,4 +1,15 @@
 angular.module('video-player')
-.component('videoListEntry', {
-  // TODO
-});
+
+  .component('videoListEntry', {
+    template: `
+      <div class="video-list-entry media">
+        <div class="media-left media-middle">
+          <img class="media-object" src="#">
+        </div>
+        <div class="media-body">
+          <div class="video-list-entry-title">TITLE HERE</div>
+          <div class="video-list-entry-detail">DESCRIPTION HERE</div>
+        </div>
+      </div>
+    `
+  });
