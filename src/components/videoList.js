57d87d7cbae7287cf5e98a8860a3a0af3dfe7990
Katelyn Stamas <cakelyn@gmail.com>
2017-10-27 16:40:32 -0400

    rendered videoList.js

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,13 @@
 angular.module('video-player')
-.component('videoList', {
-  // TODO
-});
+
+  .component('videoList', {
+    template: `
+      <div class="video-list">
+        <video-list-entry></video-list-entry>
+        <video-list-entry></video-list-entry>
+        <video-list-entry></video-list-entry>
+        <video-list-entry></video-list-entry>
+        <video-list-entry></video-list-entry>
+      </div>
+    `
+  });
