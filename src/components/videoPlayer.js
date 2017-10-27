3eebaa1ee4dd36306a387c719ec2c19c5cf18034
Katelyn Stamas <cakelyn@gmail.com>
2017-10-27 16:41:28 -0400

    rendered videoPlayer.js

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,15 @@
 angular.module('video-player')
 
-.component('videoPlayer', {
-  // TODO
-});
+  .component('videoPlayer', {
+    template: `
+      <div class="video-player">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Z6m39NS_6l0" allowFullScreen></iframe>
+        </div>
+        <div class="video-player-details">
+          <h3>TITLE</h3>
+          <div>DESCRIPTION</div>
+        </div>
+      </div>
+    `
+  });
