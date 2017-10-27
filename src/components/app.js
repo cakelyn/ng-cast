a97d3186de920faf3bafe204e582f7fbb92380da
Katelyn Stamas <cakelyn@gmail.com>
2017-10-27 16:39:23 -0400

    rendered app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,21 @@
 angular.module('video-player')
 
-.component('app', {
-  // TODO
-});
+  .component('app', {
+    template: `
+      <div>
+        <nav class="navbar">
+          <div class="col-md-6 offset-md-3">
+            <search class="navbar"></search>
+          </div>
+        </nav>
+        <div class="row">
+          <div class="col-md-7">
+            <video-player></video-player>
+          </div>
+          <div class="col-md-5">
+            <video-list></video-list>
+          </div>
+        </div>
+      </div>
+    `
+  });
